e430b0ae765ad6f4a781c2669be1f45677501c9c
Heavens Milien <heavens.milien@gmail.com>
2020-05-08 12:51:19 -0400

    Initial commit

---
+++ course-02/exercises/udacity-c2-restapi/www/config/config.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = {
+    "dev": {
+        "username": "postgres",
+        "password": "lv51xs99",
+        "database": "udagram",
+        "host": "udagram.cqqcnqe0l1lk.us-east-1.rds.amazonaws.com",
+        "dialect": "postgres",
+        "aws_region": "us-east-1",
+        "aws_profile": "default",
+        "aws_media_bucket": "udagrambuckethm"
+    },
+    "prod": {
+        "username": "",
+        "password": "",
+        "database": "udagram_prod",
+        "host": "",
+        "dialect": "postgres"
+    }
+};
+//# sourceMappingURL=config.js.map
